47c23f6e1ff26589568be0e1784adeaf83e63cc6
TriHardStudios <trihard.studios@outlook.com>
2018-09-06 10:25:34 -0600

    Test Class Created

---
+++ TestClass/CPP FIles/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello This is a test class for the 2018 TSA Software!" << endl;
+	cout << "Press any key to exit";
+	cin.get():
+	return 0;
+}
