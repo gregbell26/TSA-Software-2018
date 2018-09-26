3d76424297380e1caeb24411a70a3561de884849
jkmosa01 <jordanmosakowski@gmail.com>
2018-09-26 15:32:11 -0600

    added list button to console.log list of items in scene

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+var shapes = [];
 var x = 1;
 var y = 1;
 var z = 1;
@@ -11,13 +11,13 @@ document.getElementById("mainWindow").appendChild( renderer.domElement );
 
 var geometry = new THREE.BoxGeometry( 2,2,1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
+shapes[0] = new THREE.Mesh( geometry, material );
 var geometry2 = new THREE.BoxGeometry( 1, 1,1);
 var material2 = new THREE.MeshBasicMaterial( { color: 0x8000ff } );
-var cube2 = new THREE.Mesh( geometry2, material2 );
-scene.add( cube );
-scene.add(cube2);
-var colorChange=false
+shapes[1] = new THREE.Mesh( geometry2, material2 );
+scene.add(shapes[0]);
+scene.add(shapes[1]);
+var colorChange=false;
 camera.position.z = 5;
 var move = 0.01;
 var animate = function () {
@@ -28,16 +28,16 @@ var animate = function () {
         console.log("done")
         cube2.material.color.setHex( 0xff0000 );
     }*/
-    cube.rotation.x += move;
-    cube.rotation.y += move;
-    cube2.rotation.x += move;
-    cube2.rotation.y += move;
+    shapes[0].rotation.x += move;
+    shapes[0].rotation.y += move;
+    shapes[1].rotation.x += move;
+    shapes[1].rotation.y += move;
     /*cube2.scale.x+=0.01;
     cube2.scale.y+=0.01;
     cube2.scale.z+=0.01*/
-    cube2.scale.x=x;
-    cube2.scale.y=y;
-    cube2.scale.z=z;
+    shapes[1].scale.x=x;
+    shapes[1].scale.y=y;
+    shapes[1].scale.z=z;
 
     renderer.render( scene, camera );
 };
@@ -55,3 +55,9 @@ function changeBoxDims(dimension,value){
 
 }
 animate();
+
+function showList(){
+    for (var i=0; i<shapes.length; i++){
+        console.log("Shape "+(i+1)+": "+shapes[i]['geometry']['type'])
+    }
+}
