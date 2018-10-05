1dce685f8cdb99b767c8b05196ab395803effbb0
jkmosa01 <jordanmosakowski@gmail.com>
2018-10-05 12:38:47 -0600

    changed cube rotation to camera rotation
    cleaner code!

--- animate.js
+++ animate.js
@@ -1,11 +1,14 @@
 //core code of movement and stuff here.
+var turnSpeed = 0.0;
+var radius = 10;
 var animate = function () {
     requestAnimationFrame( animate );
-    //Animation settings
+    turnSpeed+=1;
+    camera.position.x = radius * Math.sin( THREE.Math.degToRad( turnSpeed ) );
+    camera.position.y = radius * Math.sin( THREE.Math.degToRad( turnSpeed ) );
+    camera.position.z = radius * Math.cos( THREE.Math.degToRad( turnSpeed ) );
+    camera.lookAt(scene.position);
     for (var i=0; i<shapes.length; i++){
-        shapes[i].rotation.x+=move;
-        shapes[i].rotation.y+=move;
-        shapes[i].rotation.z+=move;
         shapes[i].scale.x=scales[i][0];
         shapes[i].scale.y=scales[i][1];
         shapes[i].scale.z=scales[i][2];
