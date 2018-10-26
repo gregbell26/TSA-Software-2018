fd72b30d009ce8e9fda72e80101ff7c433a2eb39
jkmosa01 <jordanmosakowski@gmail.com>
2018-10-26 09:22:18 -0600

    added basic json pushing on addFrame (doesn't actually do anything yet)

--- keyframes.js
+++ keyframes.js
@@ -1,5 +1,13 @@
 function addFrame(){
-
+    keyFrames.push(
+        {
+            duration: 5000,
+            shapes: shapes,
+            xPosition: xPosition,
+            yPosition: yPosition,
+            zPosition: zPosition,
+        }
+    );
 }
 
 function removeFrame(){
