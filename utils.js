4cc854714e944807cb1d5e9e9e1b47f5c6349126
jkmosa01 <jordanmosakowski@gmail.com>
2018-10-31 15:15:13 -0600

    created storage.js

--- utils.js
+++ utils.js
@@ -73,26 +73,3 @@ function moveShape(dimension, value){
     }
 
 }
-
-function save(){
-    localStorage.setItem('keyFrames',JSON.stringify(keyFrames));
-    localStorage.setItem('shapes',JSON.stringify(convertShapeObjs()));
-    localStorage.setItem('scales',JSON.stringify(scales));
-    console.log('saved')
-}
-
-function convertShapeObjs(){
-    var arr= [];
-    for (var i=0; i<shapes.length; i++){
-        arr.push({
-            type: shapes[i].geometry.type,
-            positionX:shapes[i].position.x,
-            positionY:shapes[i].position.y,
-            positionZ:shapes[i].position.z,
-            r: shapes[i].material.color.r,
-            g: shapes[i].material.color.g,
-            b: shapes[i].material.color.b,
-        })
-    }
-    return arr;
-}
