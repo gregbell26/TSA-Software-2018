231df1fb28d219326d647b6871b59363b93f8969
jkmosa01 <35510468+jkmosa01@users.noreply.github.com>
2018-11-07 16:23:55 -0700

    borders on shapes are done, close #12

--- toBeSorted.js
+++ toBeSorted.js
@@ -8,18 +8,30 @@ function toggleColorBorder(checked){
 
 }
 
-
-var screen = document.getElementById("mainWindow");
-screen.onmousedown = function(event){
-    function dragScreen(event){
-        xPosition += movementX;
-        yPosition += movementY;
-    }
-
-    document.addEventListener('mousemove', dragScreen);
-
-    ball.onmouseup = function() {
-        document.removeEventListener('mousemove', dragScreen);
-        ball.onmouseup = null;
-    };
-}
+var xStart = 0;
+var yStart = 0;
+var xPosStart = xPosition;
+var yPosStart = yPosition;
+var mouseDown = false;
+$(document).on('mousedown',function(e){
+    console.log('mouse down')
+    xStart = e.pageX;
+    yStart = e.pageY;
+    console.log(xStart);
+    console.log(yStart);
+    mouseDown = true;
+})
+$(document).on('mouseup',function(e){
+    console.log('mouse up')
+    mouseDown=false;
+})
+$(document).ready(function(){
+    $(document).on('mousemove', function(e){
+        console.log('mousemove')
+        if(mouseDown){
+            console.log(true);
+            xPosition = xPosStart - (e.pageX-xStart)/10;
+            yPosition = yPosStart - (e.pageY-yStart)/10;
+        }
+    });
+});
