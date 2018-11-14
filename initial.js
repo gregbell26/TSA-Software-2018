1d35c394cfb6371b8cc10e4c07acfc7ed3aa2726
jimmybolt <44655345+jimmybolt@users.noreply.github.com>
2018-11-14 16:31:43 -0700

    Update initial.js

--- initial.js
+++ initial.js
@@ -1,41 +1,44 @@
 //jordan's code
-var x = 1;
-var y = 1;
-var z = 1;
-var animationRunning = false;
-var animationTimer;
-var selectedShape = -1;
-var scene = new THREE.Scene();
+var animationRunning = false;//self explainitory
+var animationTimer;// NI
+var selectedShape = -1;// The index of the selected shape (in shapes[])
+var scene = new THREE.Scene();// The THREE.Scene, object
 scene.background = new THREE.Color("#000000");
 var camera = new THREE.PerspectiveCamera( 75, document.getElementById("mainWindow").offsetWidth/document.getElementById("mainWindow").offsetHeight, 0.1, 1000 );
-var theta = 0;
-var renderer = new THREE.WebGLRenderer({logarithmicDepthBuffer: true });
+// the THREE camera object
+var renderer = new THREE.WebGLRenderer({logarithmicDepthBuffer: true });// the THREE render object
 
 
 renderer.setSize( document.getElementById("mainWindow").offsetWidth, document.getElementById("mainWindow").offsetHeight );
 document.getElementById("mainWindow").appendChild( renderer.domElement );
-
-var shapes = [];
-var scales = [];
-var keyFrames = [];
-var borders = [];
+//TODO turn all this stuff ArrayLists
+var shapes = [];//array of shape objects
+var scales = [];//array for shape scaling
+//TODO try to store in shape objects
+var keyFrames = [];//array for animation Keyframes
+var borders = [];//array for borders of shape
 if(localStorage.getItem('keyFrames')==null){
     newCube(2,2,2,0,0,0,"#00FF00","#000000");
     newCube(3,3,1,0,0,0,"#8000FF","#8000FF");
-}
+}//TODO GET RID OF THIS!!!
 else{
     //shapes = JSON.parse(localStorage.getItem('shapes'));
-    shapes = [];
+    shapes = []
     borders = [];
     selectedShape = 0;
-    var shapeData = JSON.parse(localStorage.getItem('shapes'));
+    //getting from storage
+    var shapeData = JSON.parse(localStorage.getItem('shapes'));//the shapes you got from Storage
     keyFrames = JSON.parse(localStorage.getItem('keyFrames'));
     scales = JSON.parse(localStorage.getItem('scales'));
     console.log(shapeData);
+    var type;
+    var newGeometry;
+    var borderGeometry;
+    var edges;
+    var borderToAdd;
+    const newMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
     for(var i=0; i<shapeData.length; i++){
-        var type = shapeData[i].type;
-        var newGeometry;
-        var borderGeometry;
+        type = shapeData[i].type;
         if (type =="TextGeometry") {
             console.log('text');
             var loader = new THREE.FontLoader();
@@ -62,6 +65,7 @@ else{
             } );
         }
         else {
+            //TODO make better
             if (type == "BoxGeometry") {
                 newGeometry = new THREE.BoxGeometry(1, 1, 1);
                 borderGeometry = new THREE.BoxBufferGeometry(1, 1, 1);
@@ -107,13 +111,12 @@ else{
                 borderGeometry = new THREE.SphereBufferGeometry(0.5, 100, 100);
                 newGeometry.name = "sphere"
             }
-            const newMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
+            //TODO make it so it dosen't copy 
             shapes[shapes.length] = new THREE.Mesh(newGeometry, newMaterial);
-            var edges = new THREE.EdgesGeometry( borderGeometry );
-            var borderToAdd = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xffffff } ) );
+            edges = new THREE.EdgesGeometry( borderGeometry );
+            borderToAdd = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xffffff } ) );
             borders.push(borderToAdd);
             scene.add( borderToAdd );
-
             scene.add(shapes[shapes.length - 1]);
             shapes[selectedShape].position.x = shapeData[i].positionX;
             shapes[selectedShape].position.y = shapeData[i].positionY;
