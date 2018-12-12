802f46ab37176f323b5334fe724fba5e6accef92
jimmybolt <44655345+jimmybolt@users.noreply.github.com>
2018-12-12 16:14:53 -0700

    Update toBeSorted.js

--- toBeSorted.js
+++ toBeSorted.js
@@ -50,7 +50,7 @@ $(document).ready(function(){
                 cameraRz -= Math.PI;
             
             if(xPosition != 0 && zPosition != 0)
-                cameraRy = Math.atan(yPosition/Math.POW(Math.POW(xPosition,2)+Math.POW(xPosition,2),.5));
+                cameraRy = Math.atan(yPosition/Math.pow(Math.pow(xPosition,2)+Math.pow(xPosition,2),.5));
             else if(yPosition > 0)
                 cameraRy = Math.PI;
             else if(yPosition < 0)
@@ -59,9 +59,9 @@ $(document).ready(function(){
             cameraRz += MvX;
             cameraRy += MvY;
             
-            xPosition = zoom * cos(cameraRz);
-            yPosition = zoom * sin(cameraRy);
-            zPosition = zoom * sin(cameraRz);
+            xPosition = zoom * Math.cos(cameraRz);
+            yPosition = zoom * Math.sin(cameraRy);
+            zPosition = zoom * Math.sin(cameraRz);
         }
     });
 });
