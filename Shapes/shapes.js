c8bdcceffe7b5fa9a282c2a574c3ebeba5442fce
jimmybolt <44655345+jimmybolt@users.noreply.github.com>
2019-01-09 17:14:50 -0700

    Fixed movement errors

--- Shapes/shapes.js
+++ Shapes/shapes.js
@@ -1,11 +1,15 @@
 -//corbin wrote this method, simplified by Jordan
+
 function moveShape(dimension, value) {
     shapes[selectedShape].position[dimension] = Number(value);
     borders[selectedShape].position[dimension] = Number(value);
 }
 
 
-
+function postitionShape(dimension, value) {
+    shapes[selectedShape].position[dimension] = Number(value);
+    borders[selectedShape].position[dimension] = Number(value);
+}
 
 function rotateShape(dimension, value) {
     shapes[selectedShape].rotation[dimension] = Number(value)*Math.PI/180;
