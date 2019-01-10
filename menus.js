06a94a79eb54bef33ec22ba8cd8fa76adfa230f1
Corbin Estes <cwestes01@bvsd.org>
2019-01-09 18:02:06 -0700

    Fixed 1s on create bug

--- menus.js
+++ menus.js
@@ -28,9 +28,9 @@ function setSelectedShape(num){
     document.getElementById('rotateBoxY').value = (shapes[selectedShape].rotation.y*180/Math.PI);
     document.getElementById('rotateBoxZ').value = (shapes[selectedShape].rotation.z*180/Math.PI);
     //This is causing the 1 at creation problem
-    document.getElementById('dimensionX').value = shapes[selectedShape].scale.x;
-    document.getElementById('dimensionY').value = shapes[selectedShape].scale.y;
-    document.getElementById('dimensionZ').value = shapes[selectedShape].scale.z;
+    document.getElementById('dimensionX').value = scales[selectedShape][0];
+    document.getElementById('dimensionY').value = scales[selectedShape][1];
+    document.getElementById('dimensionZ').value = scales[selectedShape][2];
     editMenu();
     console.log("Set Shape Num");
 }
