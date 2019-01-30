e86fa1b6bc2bec868f3ae9a3239e04c13387b2fe
EddsBed <dtfredrick@gmail.com>
2019-01-30 11:03:30 -0700

    Update tutorial.js

--- tutorial.js
+++ tutorial.js
@@ -1,12 +1,13 @@
 var usingTutorial = false;
-
+//the default of loading tutorial is false. 
+//Tutorial will load on the call of this function
 function launchTutorial(){
-    document.getElementById("welcomeScreen").style.display = "none";
-    document.getElementById('tutorialArrow').style.display = "inherit";
+    document.getElementById("welcomeScreen").style.display = "none"; //removes welcome screen
+    document.getElementById('tutorialArrow').style.display = "inherit"; //launches tutorial arrow in the current scene
     setTimeout(function(){
         if(confirm("To create a shape, click on the button with the cube")){
-            animateArrow(-60,15,106,120);
-            usingTutorial = true;
+            animateArrow(-60,15,106,120); //brings arrow to the position
+            usingTutorial = true; 
         }
     }, 10);
 
