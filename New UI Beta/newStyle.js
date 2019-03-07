4215262b55cc31c754c0f7cf5c7e85b2e89beaf5
trihardstudios <trihard.studios@outlook.com>
2019-03-06 22:08:42 -0700

    Started UI Overhual. Base for welcome screen is done

---
+++ New UI Beta/newStyle.js
@@ -0,0 +1,8 @@
+/*
+*
+* This is the new style for the Animation program.
+* Will include dark Mode Detector
+*
+*
+*
+* */
---
+++ statusBox.js
@@ -0,0 +1,8 @@
+/*
+* This will be the status box that can report errors and such
+* All console output shall go here as to allow tobe disabled when
+*
+*
+*
+*
+* */
