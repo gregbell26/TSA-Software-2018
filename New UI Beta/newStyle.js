d82e195ab99c7e7fe4d6cc05283bc012ba29057f
trihardstudios <trihard.studios@outlook.com>
2019-03-20 15:18:41 -0600

    Worked on buttons

--- New UI Beta/newStyle.js
+++ New UI Beta/newStyle.js
@@ -8,7 +8,7 @@
 * */
 
 
-
+//----------------------------CLASS-ADDERS-----------------------------------------
 /*
 * Adds class to hide the welcome screen in a cool animation
 * */
@@ -17,7 +17,32 @@ function hideWelcomeScreen(){
     document.getElementById("ws_body").classList.add("ws_hide");
 }
 
+/*
+* Shows and hides menus in a more elegant fashion
+* */
+var activeMenu ="init";
+function showMenu(menuToShow){
+    if(activeMenu !== "init") {
+        document.getElementById(activeMenu).classList.remove("menu_show");
+        document.getElementById(activeMenu).classList.add("menu_hidden");
+    }
 
+    document.getElementById(menuToShow).classList.remove("menu_hidden");
+    document.getElementById(menuToShow).classList.add("menu_show");
+
+    activeMenu = menuToShow;
+}
+
+var toggledButtons={};
+function timelineButtonToggle(buttonToToggle){
+}
+
+function showTimeLine(){
+    document.getElementById("std_timeline").classList.add("timeline_show");
+}
+
+
+//---------------------------------PSEUDO-FUNCTIONS-----------------------------------------
 /*
 * Pseudo function for populating list with save names
 * Need to tie in with saveSubSystem
@@ -34,26 +59,53 @@ function loadSaveNames() {
 }
 
 
-var activeMenu ="init";
-function showMenu(menuToShow){
-    if(activeMenu !== "init") {
-        document.getElementById(activeMenu).classList.remove("menu_show");
-        document.getElementById(activeMenu).classList.add("menu_hidden");
-    }
 
-    document.getElementById(menuToShow).classList.remove("menu_hidden");
-    document.getElementById(menuToShow).classList.add("menu_show");
-
-    activeMenu = menuToShow;
-
-}
-function darkModeLoader(arg){
+//----------------------------STYLE-FUNCTIONS--------------------------------------------------
+/*
+* Dynamically loads style sheets and allows for different themes to be easily created
+* */
+var stylesheetLoaded = false;
+var loadedStyleTag= "";
+function stylesheetLoader(stylesheetName){
+    var validStyleSheet=false;
     var cssToLoad=document.createElement("link");
-    if(arg === "DEVLOCK"){
-        cssToLoad.setAttribute("rel", "stylesheet");
-        cssToLoad.setAttribute("type", "text/css");
+    cssToLoad.setAttribute("rel", "stylesheet");
+    cssToLoad.setAttribute("type", "text/css");
+
+    if(stylesheetLoaded){
+        document.getElementsByTagName("head")[0].removeChild(loadedStyleTag);
+        stylesheetLoaded=false;
+        loadedStyleTag = "";
+
+    }
+    if(stylesheetName === "normalMode"){
         cssToLoad.setAttribute("href", "normalMode_Style.css");
-        document.getElementsByTagName("head")[0].appendChild(cssToLoad);
+        validStyleSheet = true;
     }
 
+    else if (stylesheetName ==="darkMode"){
+        cssToLoad.setAttribute("href", "darkMode_Style.css");
+        validStyleSheet =true;
+    }
+
+    else if(stylesheetName === "mobile"){
+        cssToLoad.setAttribute("href", "mobileMode_Style.css");
+        validStyleSheet = true;
+    }
+    else {
+        console.log("ERROR: Invalid CSS")
+    }
+
+    if(validStyleSheet) {
+        document.getElementsByTagName("head")[0].appendChild(cssToLoad);
+        stylesheetLoaded = true;
+        loadedStyleTag = cssToLoad;
+    }
 }
+/*
+* Hides things for mobile users.
+* */
+function mobileHider(){
+
+}
+
