eeef5e271cbf0bdeba4f021762b0512dc9dcd79e
EddsBed <eddsbed@gmail.com>
2019-05-30 14:39:40 -0600

    tiny changes

--- tutorial.js
+++ tutorial.js
@@ -1,19 +1,16 @@
-let usingTutorial = false;
 //the default of loading tutorial is false. 
 //Tutorial will load on the call of this function
 function launchTutorial(){
-    document.getElementById("welcomeScreen").style.display = "none"; //removes welcome screen
-    document.getElementById('tutorialArrow').style.display = "inherit"; //launches tutorial arrow in the current scene
-    setTimeout(function(){
-        if(confirm("To create a shape, click on the button with the cube")){
-            animateArrow(-60,15,106,120); //brings arrow to the position
-            usingTutorial = true; 
-        }
-    }, 10);
+  // document.getElementById("welcomeScreen").style.display = "none"; //removes welcome screen 
 
+     showPopUp("popUp_error_body","Tutorial","To begin, click on the shapes box",-1);
+
+    document.getElementById('tutorialArrow').style.display = "inherit"; //launches tutorial arrow in the current scene
+
+     animateArrow(15, 75, 120, 120);
 }
 
-let animateArrowTime;
+var animateArrowTime;
 function animateArrow(startT, endT, startL, endL){
     animateArrowTime = 0;
     let arrowAnimate = setInterval( function(){
@@ -22,6 +19,7 @@ function animateArrow(startT, endT, startL, endL){
             document.getElementById('tutorialArrow').style.top = startT + (endT-startT)/200*animateArrowTime + "px";
             document.getElementById('tutorialArrow').style.left = startL + (endL-startL)/200*animateArrowTime + "px";
         }
+
         else{
             clearInterval(arrowAnimate);
             document.getElementById('tutorialArrow').style.top = endT + "px";
