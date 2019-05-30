b7bb59ec42af7ec534898044739169ddc29ec329
EddsBed <eddsbed@gmail.com>
2019-05-30 15:11:16 -0600

    test

--- tutorial.js
+++ tutorial.js
@@ -1,29 +1,21 @@
+import { getDiffieHellman } from "crypto";
+
 //the default of loading tutorial is false. 
 //Tutorial will load on the call of this function
 function launchTutorial(){
   // document.getElementById("welcomeScreen").style.display = "none"; //removes welcome screen 
 
-     showPopUp("popUp_error_body","Tutorial","To begin, click on the shapes box",-1);
+    showPopUp("popUp_error_body","Tutorial","To begin, click on the shapes box",-1);
 
-    document.getElementById('tutorialArrow').style.display = "inherit"; //launches tutorial arrow in the current scene
+    document.getElementById('tutorialArrow').style.display = "block"; //launches tutorial arrow in the current scene
 
-     animateArrow(15, 75, 120, 120);
+    animateArrow(120, 120);
 }
 
-var animateArrowTime;
-function animateArrow(startT, endT, startL, endL){
-    animateArrowTime = 0;
-    let arrowAnimate = setInterval( function(){
-        animateArrowTime+=7.5;
-        if(animateArrowTime<200){
-            document.getElementById('tutorialArrow').style.top = startT + (endT-startT)/200*animateArrowTime + "px";
-            document.getElementById('tutorialArrow').style.left = startL + (endL-startL)/200*animateArrowTime + "px";
-        }
+function animateArrow(x, y){
+    document.getElementbyId(tutorialArrow).style.left=toString(x)+"px";
+    document.getElementbyId(tutorialArrow).style.top=toString(y)+"px";
+  
 
-        else{
-            clearInterval(arrowAnimate);
-            document.getElementById('tutorialArrow').style.top = endT + "px";
-            document.getElementById('tutorialArrow').style.left = endL + "px";
-        }
-    }, 10);
 }
+
