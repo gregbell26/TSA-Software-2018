3f9fc3a35f6e8cd256ceb4f14ce6447b0b413a71
trihardstudios <trihard.studios@outlook.com>
2019-05-30 16:13:04 -0600

    Worked on torturial

--- tutorial.js
+++ tutorial.js
@@ -1,20 +1,19 @@
-import { getDiffieHellman } from "crypto";
-
-//the default of loading tutorial is false. 
+//the default of loading tutorial is false.
 //Tutorial will load on the call of this function
 function launchTutorial(){
-  // document.getElementById("welcomeScreen").style.display = "none"; //removes welcome screen 
-
     showPopUp("popUp_error_body","Tutorial","To begin, click on the shapes box",-1);
 
     document.getElementById('tutorialArrow').style.display = "block"; //launches tutorial arrow in the current scene
 
     animateArrow(120, 120);
+
 }
 
 function animateArrow(x, y){
-    document.getElementbyId(tutorialArrow).style.left=toString(x)+"px";
-    document.getElementbyId(tutorialArrow).style.top=toString(y)+"px";
+    document.getElementById("tutorialArrow").style.left=x.toString()+"px";
+    document.getElementById("tutorialArrow").style.top=y.toString()+"px";
+
+
   
 
 }
